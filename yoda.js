b965a34d5f1a43d9c0d18be16db1cf8c81a65b1f
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-05-08 09:48:53 -0400

    ajax is failing, can print user input to console on 'return'

--- yoda.js
+++ yoda.js
@@ -1,5 +1,31 @@
-document.querySelector("#userInput").addEventListener("keyup", function( event ){
-  event.preventDefault()
-  if (event.keyCode == 13)
-    console.log("excellent")
+$(document).ready(function(){
+
+var userInput = document.querySelector("#userInput")
+
+  userInput.addEventListener( "keyup", function( event ){
+    event.preventDefault()
+    if( event.keyCode == 13 ){
+      console.log( userInput.value )
+    }
+  })
+
+
+  var yodaURL = "https://yoda.p.mashape.com/yoda?sentence="
+  var inputPhrase = "super+duper"
+
+  $.ajax({
+    url:  yodaURL + inputPhrase,
+    headers: {
+      "X-Mashape-Key": "OGPgEThqxtmshCpTDZyOUBndDjLLp1Lm0qcjsnpxxdQawmnaj7"
+    },
+    timeout: 6000,
+    type: "GET",
+    dataType: "json"
+  }).done( function( response ){
+    console.log( "you did it" )
+  }).fail( function( response ){
+    console.log( "ajax fail" )
+  })
+
+
 })
