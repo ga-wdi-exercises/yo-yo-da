0fb41ca41d198869e857454db603add533de70b3
Don McLamb <mclamb.donald@gmail.com>
2015-05-07 22:47:10 -0400

    completness 3 comfort 3, added an extra closing bracker or something, can't find it too tired

--- yoda.js
+++ yoda.js
@@ -1,21 +1,59 @@
 
-var defaultInput = $("#userInput");
+var userInput = $("#userInput").val();
+var textArea = $("#userInput")
+
+
+$(textArea).keypress(function(event){
+console.log("test")
+  var enter = event.which;
+
+  if(enter === 13){
+    newAPICall = new YodaAPI(userInput);
+  }
+});
+
 
 // input.on("click", replaceText(input))
 
+//
+//
+// $(function(){
+//   $("#userInput").on("click", function(input){
+//
+//     function replaceText(input){
+//       defaultInput.preventDefault();
+//       defaultInput.remove();
+//       // input.empty();
+//
+//       // $(body).append($('<input type="text" id="yodatalk"' + this.innerHTML + '</input>'));
+//       var userInput1  =  input.val();
+//       return userInput1
+//     }
+//
+//
+//   } );
+//
+//
+//
+//
+// })
+
+function YodaAPI(userInput){
+
+  var yodaOutput = $("#yodaOutput").val();
+
+  yodaOutput.val()
+
+};
 
 
-$(function(){
-  $("#userInput").on("click", replaceText(input));
+YodaAPI.prototype ={
+  api: function(userInput)
+  {$.ajax({
+    url: "https://yoda.p.mashape.com/yoda?sentence=" + userInput,
+    headers:{
+      "X-Mashape-Key": "OGPgEThqxtmshCpTDZyOUBndDjLLp1Lm0qcjsnpxxdQawmnaj7"
+    }
+  });
 
-  function replaceText(input){
-    input.preventDefault();
-    input.remove();
-    // input.empty();
-    input.append($('<input type="text" id="yodatalk"' + this.innerHTML + '</input>'));
-    var userInput  =  input.val();
-    return
-  }
-
-
-})
+}
